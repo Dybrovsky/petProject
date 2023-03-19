f8ba188ecc1f7c3901fe8ef79a0f18e9e6216529
Dybrovsky <sceven71@gmail.com>
2023-03-19 13:31:00 +0200

    welcomepage w/o store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import PopUp from "./components/popUp/PopUp";
+import WelcomePage from "./components/welcomePage/WelcomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <WelcomePage/>
+            <PopUp/>
+        </div>
+    );
 }
 
 export default App;
